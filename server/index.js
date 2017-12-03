a5f6b1b730af0f0c0b16ecaf87a5e630bbeacf62
UHanczar <u.hanczar@gmail.com>
2017-12-03 22:33:56 +0300

    Handling_payments_and_adding_routing_for_client

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 require('./models/User');
@@ -10,6 +11,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,10 +23,22 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'Hello, there!' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file if it doesn't recognize the file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
